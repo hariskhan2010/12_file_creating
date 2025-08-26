1de012c211c1a046b9537dd313f54d0e9852e301
hariskhan2010 <harismalik2010afridi@gmail.com>
2025-08-26 11:13:27 +0500

    Add files via upload

---
+++ 12_file_creating.py
@@ -0,0 +1,57 @@
+# file_opening = open("harismalik.py", "w")
+# text = "print(\"helloworld\")"
+# file_opening .write(text)
+# print(text)
+# file_opening.close()
+
+# file_openning = open("harismalik.py", "w")
+# text = "print(\"helloworld\")"
+# file_openning.write(text)
+# file_openning.close()
+
+# file_opening = open("hariskhan.txt", "w")
+# text = "this is my notebook."
+# file_opening.write(text)
+# file_opening.close()
+
+# file_reading = open("02_variables.py", "r")
+# text = file_reading.read()
+# print(text)
+# file_reading.close()
+
+# file_openning = open("haris_malik.py", "w")
+# text = "print(\"helloworld\")"
+# file_openning.write(text)
+# file_openning.close()
+
+# file_openning = open("haris_malik.py","r")
+# text = file_openning.read()
+# print(text)
+# file_openning.close
+
+# with open("notes.txt", "w") as file:
+#     file.write("this is my notes")
+
+# try:
+#     with open("notesn.txt", "r") as mynotes:
+#         print(mynotes.read())
+# except FileNotFoundError:
+#     print("file not found")
+
+# try:
+#     with open("notes.txt", "r") as mynotes:
+#         print(mynotes.read())
+# except FileNotFoundError:
+#     print("create a file first")
+
+
+
+# try:
+#     with open("01_helloworld.py", "r") as file:
+#         print(file.read())
+# except FileNotFoundError:
+#     print("create first a file")
+
+
+# with open("haris_diary.txt", "w") as file:
+#     file.write("hello this is my diary")
